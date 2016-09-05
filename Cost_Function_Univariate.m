093e2b7a8bdd4edce42499599154c12edda878f8
Stephon94 <micronixon30@gmail.com>
2016-09-05 16:15:06 -0400

    Update Cost_Function_Univariate.m

--- Cost_Function_Univariate.m
+++ Cost_Function_Univariate.m
@@ -1,7 +1,10 @@
-function J = (theta, X, y)
+%Initializes the Cost Function
+function J =  UniCostFunction(theta, X, y)
 
+%Sets the amount of rows in the training set to "m"
 m = length(y);
 
-J = 0;
 
 J = 1/(2 * m) * sum(((X*theta) .-y).^2);
+
+end
